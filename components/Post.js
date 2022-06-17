5ce027c8fb480b0b6b6aa0b5403cde2163d17469
Taylor Dean <taylordean787@gmail.com>
2022-06-16 20:44:25 -0500

    fixed classnames

--- components/Post.js
+++ components/Post.js
@@ -2,17 +2,17 @@ import Link from "next/link";
 
 export default function Post({ post }) {
   return (
-    <div class="card">
+    <div className="card">
       <img src={post.frontmatter.cover_image} alt="" />
 
-      <div class="post-date">Posted on {post.frontmatter.date}</div>
+      <div className="post-date">Posted on {post.frontmatter.date}</div>
 
       <h3>{post.frontmatter.title}</h3>
 
       <p>{post.frontmatter.excerpt}</p>
 
       <Link href={`/blog/${post.slug}`}>
-        <a class="btn">Read More</a>
+        <a className="btn">Read More</a>
       </Link>
     </div>
   );
